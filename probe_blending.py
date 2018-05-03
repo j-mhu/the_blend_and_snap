beb8aaa8ed6d6c8a2af91a649c29396d487c7e5b
Helena Wu <helenawu1998@gmail.com>
2018-05-03 11:41:23 -0700

    Implement probe blending with simple linear regression

--- probe_blending.py
+++ probe_blending.py
@@ -7,16 +7,19 @@ from numpy.linalg import inv
 
 def get_data():
     '''
-    Loads prediction data from desired models for blending.
+    Loads probe and prediction data from desired models for blending.
 
     Returns matrix A = [A_0, A_1, A_2, ...A_M-1] where A_i is the predictions
-    from the ith model.
+    from the ith model on probe.
     '''
-    print('Loading qual predictions...')
+    print('Loading probe data...')
+    probe = np.loadtxt('../data/um/probe.dta').astype(int)
+    probe = np.delete(probe, 2, axis=1)
+    print('Done loading probe data.')
+    print('Loading probe predictions...')
+    models = ['./out/threes.dta', './out/fours.dta']
     # models = ['./out/svd_funny_2.dta', './out/svd++1.dta']
-    models = ['./out/svd_funny_2.dta', './out/svd4.dta']
-    RMSEs = [.91903, 0.94985]
-    # RMSEs = [.91903, 0.97918]
+    # models = ['./out/svd_funny_2.dta', './out/svd4.dta']
 
     for i, model in enumerate(models):
         A_model = np.loadtxt(model).astype(int)
@@ -25,37 +28,30 @@ def get_data():
         else:
             A = np.append(A, [A_model], axis=0)
     A = A.T
-    print('Done loading qual predictions.')
-    return A, RMSEs
+    print('Done loading probe predictions.')
+    return A, probe
 
-def find_coefficients(A, RMSEs):
+def find_coefficients(A, probe):
     '''
-    Uses technique from given slides to find optimal alphas for blending on quiz.
+    Uses simple linear regression to find optimal alphas for blending on probe.
 
     Returns matrix alphas, the optimal coefficients corresponding to the ith model
     for linear combination.
     '''
-    NUM_POINTS = 2749898
-    zeroes_RMSE = 3.84358 * 3.84358 * NUM_POINTS
-    new_RMSEs = [math.pow(r, 2) * NUM_POINTS for r in RMSEs]
-    ATs = []
-    for i, A_i in enumerate(A.T):
-        ATs.append(0.5 * (np.dot(A_i, A_i) + zeroes_RMSE - new_RMSEs[i]))
-        # ATs.append(0.5 * (np.dot(A_i, A_i) + zeroes_RMSE * zeroes_RMSE - RMSEs[i]))
-
-    alphas = np.matmul(inv(np.matmul(A.T, A)), ATs)
-    print(alphas)
+    alphas = list(np.dot(np.linalg.pinv(A), probe[:, 2]))
+    print('Optimal coefficients: ', alphas)
     return alphas
 
-def generate_new_predictions(alphas, A):
+def generate_new_predictions(alphas, A, probe):
     '''
     Use linear coefficients to generate new predictions with a linear blend.
 
     Writes new predictions to file.
     '''
-    with open('./out/probe_blend_(funny2_svd4).dta', 'w+') as new:
+    with open('./out/probe_blend_threes_fours.dta', 'w+') as new:
+        error = 0
         count = 0
-        for predictions in A:
+        for i, predictions in enumerate(A):
             new_val = 0
             for j in range(len(alphas)):
                 new_val += alphas[j] * predictions[j]
@@ -63,12 +59,16 @@ def generate_new_predictions(alphas, A):
                 new_val = 1
             elif new_val > 5:
                 new_val = 5
+            error += math.pow((new_val - probe[i][2]), 2)
             new.write(str(new_val) + '\n')
             count += 1
+        error /= count
+        error = math.pow(error, 0.5)
         print('Count of number of predictions: ', count)
+        print('RMSE: ', error)
 
 
 if __name__ == "__main__":
-    A, RMSEs = get_data()
-    alphas = find_coefficients(A, RMSEs)
-    generate_new_predictions(alphas, A)
+    A, probe = get_data()
+    alphas = find_coefficients(A, probe)
+    generate_new_predictions(alphas, A, probe)
